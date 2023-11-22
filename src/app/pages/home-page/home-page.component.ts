163abc9a17a3f6c692162664adc475e81b1f87eb
vaishnavrajeev2000 <vaishnavr55555@gmail.com>
2023-11-22 14:31:11 +0530

    Vaishnav new

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiService } from '../../api.service';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
@@ -14,6 +15,7 @@ export class HomePageComponent {
   items: any;
  constructor(private api:ApiService){}
   todos:any;
+  newTaskTitle:string='';
   ngOnInit(){
     this.api.getTodo().subscribe((data:any) => {
      this.todos=data
@@ -21,6 +23,14 @@ export class HomePageComponent {
   }
   delete(id: any){
     this.todos=this.todos.filter((e : {id:any})=> e.id != id)
+  }
+  addNewTask(){
+    if (this.newTaskTitle.trim() !== ''){
+      const newTask = { title: this.newTaskTitle, finished:false};
+      this.todos.push(newTask);
+      this.newTaskTitle= '';//clear the input field after adding task
+    }
+  
   }
 
 }
